231a66839dc5db6db07a40c9347032e365ff4d9f
Mathias Polligkeit <git@woylie.de>
2023-07-20 18:29:47 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Bumper
+
+Dirty little mix task that saves you 5 seconds of your time by bumping the
+version of an Elixir project in `mix.exs`, `README.md` and `CHANGELOG.md` all
+at once.
+
+## Assumptions
+
+- The version in `mix.exs` is assigned to a module attribute (e.g.
+  `@version 0.1.0`).
+- `README.md` has installation instructions for adding the mix dependency
+  (e.g. `{:my_library, "~> 0.1.0}`).
+- The changelog uses the [keep a changelog](https://keepachangelog.com) format
+  and has an `## Unreleased` header.
+
+## Usage
+
+```bash
+mix bump patch
+mix bump minor
+mix bump major
+```
---
+++ lib/mix/task/bump.ex
@@ -0,0 +1,78 @@
+defmodule Mix.Tasks.Bump do
+  @shortdoc "Bumps the version"
+  @moduledoc """
+  Bumps the version.
+
+  ## Usage
+
+      mix bump patch
+      mix bump minor
+      mix bump major
+  """
+
+  @valid_args ["major", "minor", "patch"]
+  @changelog "CHANGELOG.md"
+  @mix_file "mix.exs"
+  @readme "README.md"
+
+  use Mix.Task
+
+  @impl Mix.Task
+  def run([arg]) when arg in @valid_args do
+    app = Keyword.fetch!(Mix.Project.config(), :app)
+    new_version = bump_version(current_version!(), String.to_existing_atom(arg))
+    update_file(@changelog, &update_changelog(&1, new_version))
+    update_file(@mix_file, &update_mix_file(&1, new_version))
+    update_file(@readme, &update_readme(&1, app, new_version))
+  end
+
+  def run(_) do
+    IO.puts(@moduledoc)
+  end
+
+  defp current_version! do
+    Mix.Project.config()
+    |> Keyword.fetch!(:version)
+    |> Version.parse!()
+  end
+
+  defp bump_version(current_version, segment) do
+    current_version
+    |> Map.put(segment, Map.fetch!(current_version, segment) + 1)
+    |> Version.to_string()
+  end
+
+  defp update_changelog(changelog, new_version) do
+    today = Date.to_iso8601(Date.utc_today())
+    unreleased_header = "## Unreleased"
+    new_header = "#{unreleased_header}\n\n## [#{new_version}] - #{today}"
+    String.replace(changelog, unreleased_header, new_header)
+  end
+
+  defp update_mix_file(mix_file, new_version) do
+    String.replace(
+      mix_file,
+      ~r/@version "[0-9.]+"/,
+      ~s(@version "#{new_version}")
+    )
+  end
+
+  defp update_readme(readme, app, new_version) do
+    app = inspect(app)
+
+    String.replace(
+      readme,
+      ~r/{#{Regex.escape(app)}, ".+"}/,
+      ~s({#{app}, "~> #{new_version}"})
+    )
+  end
+
+  defp update_file(file, fun) do
+    new_content =
+      file
+      |> File.read!()
+      |> fun.()
+
+    File.write!(file, new_content)
+  end
+end
